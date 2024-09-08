4829fd037602830b6d0b6973faf07d3e834095f3
doctorsum <omar.thomas9999@gmail.com>
2024-09-08 08:27:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def home():
     return render_template('index.html', days=days, hours=hours, minutes=minutes, seconds=seconds)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=4444, debug=False)
+    app.run(host='0.0.0.0', port=4444, debug=False)
